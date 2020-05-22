e8de461a4f506ca499953ea5c7c157d78d389b3b
Salem Owino <salemowino18@gmail.com>
2020-05-22 16:47:05 +0300

    A test for image post created

--- mygallery/tests.py
+++ mygallery/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+
+class TestImagePost(TestCase):
+
+    def setUp(self):
+        self.new_category = ImageCategory(name='nature')
+        self.new_category.save_imagecategory()
+
+        self.new_location = ImageLocation(name="Aboretum")
+        self.new_location.save_imagelocation()
+
+        self.new_image = ImagePost(image_name='hacker', image_description='This guy is a real hacker',
+                               image_location=self.new_location, image_category=self.new_category)
+        self.new_image.save_imagepost()
+
+    def test_isinstance(self):
+        self.new_image = ImagePost(image_name='hacker', image_description='This guy is a real hacker', 
+                               image_location=self.new_location,image_category=self.new_category)
+        self.new_image.save_imagepost()
+        self.assertTrue(isinstance(self.new_image, ImagePost))
