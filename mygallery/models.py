b7f9c52720cf595afadd1cb25af3fbd2c0a7748b
Salem Owino <salemowino18@gmail.com>
2020-05-22 16:51:15 +0300

    A function for image category

--- mygallery/models.py
+++ mygallery/models.py
@@ -2,6 +2,17 @@ from django.db import models
 
 # Create your models here.
 
+class ImageCategory(models.Model):
+    name = models.CharField(max_length=60)
+
+    def __str__(self):
+        return self.name
+
+    def save_imagecategory(self):
+        self.save()
+
+
+
 @classmethod
 def search_image(cls, search_term):
     images = cls.objects.filter(image_category__name=search_term)
