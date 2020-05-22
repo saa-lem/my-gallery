87e1b8cbb1377a7c61dfd9d176f79697581f041d
Salem Owino <salemowino18@gmail.com>
2020-05-22 17:06:28 +0300

    Urls for different pages provided

--- mygallery/urls.py
+++ mygallery/urls.py
@@ -5,12 +5,12 @@ from . import views
 
 urlpatterns = [
   
-    #re_path('images/',views.index, name = 'home'),
-    #path('',views.landing, name = 'landing'),
-    #path('aboretum', views.aboretum_images, name = "aboretum"),
-    #path('nature', views.nature_images, name = "nature"),
-    #path('park', views.park_images, name = "park"),
-    #path('beach', views.beach_images, name = "beach"),
-    #path('cgi', views.cgi_images, name = "cgi"),
+    re_path('images/',views.index, name = 'home'),
+    path('',views.landing, name = 'landing'),
+    path('aboretum', views.aboretum_images, name = "aboretum"),
+    path('nature', views.nature_images, name = "nature"),
+    path('park', views.park_images, name = "park"),
+    path('beach', views.beach_images, name = "beach"),
+    path('cgi', views.cgi_images, name = "cgi"),
     re_path('^search/',views.search_results, name = 'search_results'),
 ]
