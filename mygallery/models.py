65fac3be8d4be4b95f1292f19247a1d7a2e3af14
Salem Owino <salemowino18@gmail.com>
2020-05-22 14:19:15 +0300

    A function to enable user search for image

--- mygallery/models.py
+++ mygallery/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+    @classmethod
+    def search_image(cls, search_term):
+        images = cls.objects.filter(image_category__name=search_term)
+        return images
---
+++ mygallery/urls.py
@@ -0,0 +1,16 @@
+
+from django.urls import path, re_path
+from . import views
+
+
+urlpatterns = [
+  
+    #re_path('images/',views.index, name = 'home'),
+    #path('',views.landing, name = 'landing'),
+    #path('aboretum', views.aboretum_images, name = "aboretum"),
+    #path('nature', views.nature_images, name = "nature"),
+    #path('park', views.park_images, name = "park"),
+    #path('beach', views.beach_images, name = "beach"),
+    #path('cgi', views.cgi_images, name = "cgi"),
+    re_path('^search/',views.search_results, name = 'search_results'),
+]
